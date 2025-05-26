37c61bd5061a86f73f334b65b0cbc2b45196b73f
mi-wada <mtak.wd@gmail.com>
2025-05-26 16:41:34 +0900

    add custom name

--- helloworld/src/index.ts
+++ helloworld/src/index.ts
@@ -3,7 +3,19 @@ import { Hono } from "hono";
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/message", (c) => {
-  return c.text("Hello, World!🚀");
+  const name = c.req.query("name") || undefined;
+  const message = generateGreeting(name);
+  return c.text(message);
 });
+
+/**
+ * Generates a greeting message for the provided name.
+ *
+ * @param name - The name of the person to greet. If not provided, "Anonymous" will be used.
+ * @returns A greeting message string.
+ */
+const generateGreeting = (name: string = "Anonymous"): string => {
+  return `Hello, ${name}!`;
+};
 
 export default app;
