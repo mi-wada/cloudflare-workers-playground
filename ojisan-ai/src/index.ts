bc6ef9d40c34f0fb30ad7bcc7639fb256d9149a7
mi-wada <mtak.wd@gmail.com>
2025-06-03 11:47:29 +0900

    deploy

--- ojisan-ai/src/index.ts
+++ ojisan-ai/src/index.ts
@@ -1,18 +1,9 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
+		const { pathname } = new URL(request.url);
+		if (pathname !== "/api") {
+			return new Response("Not Found", { status: 404 });
+		}
 		const q = new URL(request.url).searchParams.get("q");
 		if (!q) {
 			return new Response(
